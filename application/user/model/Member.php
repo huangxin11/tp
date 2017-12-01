105adaddfe08661901c222f4500846e52a2b5e0d
huangxin <973718378@qq.com>
2017-12-01 15:39:51 +0800

    1

--- application/user/model/Member.php
+++ application/user/model/Member.php
@@ -46,7 +46,6 @@ class Member extends Model{
             $this->error = '用户未激活或已禁用！'; //应用级别禁用
             return false;
         }
- 
         /* 登录用户 */
         $this->autoLogin($user); 
         //记录行为
